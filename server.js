34db3d71200485c54cfc1b9c4246272fa902543b
Aditya200410 <adityakumar098k@gmail.com>
2026-01-11 22:33:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -39,13 +39,7 @@ const allowedOrigins = [
 ];
 
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(null, false);
-    }
-  },
+  origin: true,
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Origin'],
