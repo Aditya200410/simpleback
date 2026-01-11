e16a074aad8289a40391fa42a86e19d061846f7a
Aditya200410 <adityakumar098k@gmail.com>
2026-01-11 22:40:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -46,8 +46,8 @@ app.use(cors({
   credentials: true,
   optionsSuccessStatus: 200
 }));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use('/photo', express.static(path.join(__dirname, 'photo')));
 
 const upload = require('./middleware/upload');
